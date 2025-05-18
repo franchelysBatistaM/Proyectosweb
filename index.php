48e602e4f4316e58dd0bc3082e1df4f01cbcccb9
FranchelysBatista <211575755+franchelysBatistaM@users.noreply.github.com>
2025-05-18 13:52:15 -0400

    Creando index.php

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>menu</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #fff5f0;
+            text-align: center;
+            padding-top: 50px;
+            color: #4a4a4a;
+        }
+        ul {
+            list-style-type: none;
+            padding: 0;
+        }
+        li {
+            margin: 20px;
+        }
+        a {
+            display: inline-block;
+            text-decoration: none;
+            background-color: #f78da7;
+            color: white;
+            padding: 12px 24px;
+            border-radius: 10px;
+            font-size: 18px;
+            transition: 0.3s;
+        }
+        a:hover {
+            background-color: #c94c73;
+        }
+    </style>
+</head>
+<body>
+    <h1> menu</h1>
+    <ul>
+        <li><a href="mi_tarjeta.php">Mi Tarjeta</a></li>
+        <li><a href="calculadora.php">Calculadora</a></li>
+        <li><a href="adivina.php">Adivina mi numero</a></li>
+        <li><a href="acerca_de.php">Acerca de</a></li>
+    </ul>
+</body>
+</html>
