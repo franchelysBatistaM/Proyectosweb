28e6872a32de32500c9b1ed820f2e99dc7016d75
FranchelysBatista <211575755+franchelysBatistaM@users.noreply.github.com>
2025-05-18 13:58:08 -0400

    Creando acerca_de.php

---
+++ acerca_de.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Acerca de</title>
+    <style>
+        body {
+            font-family: Arial;
+            text-align: center;
+            background-color: #fff5f0;
+            padding: 50px;
+            color: #4a4a4a;
+        }
+        img {
+            border-radius: 50%;
+            border: 3px solid #f78da7;
+        }
+        a {
+            display: inline-block;
+            margin: 10px;
+            color: #f78da7;
+            text-decoration: none;
+            font-weight: bold;
+        }
+        a:hover {
+            color: #c94c73;
+        }
+        iframe {
+            margin-top: 30px;
+            border-radius: 10px;
+        }
+        .volver-btn {
+            display: inline-block;
+            margin-top: 50px;
+            padding: 10px 20px;
+            background: #f78da7;
+            color: white;
+            text-decoration: none;
+            border-radius: 8px;
+        }
+        .volver-btn:hover {
+            background-color: #c94c73;
+        }
+    </style>
+</head>
+<body>
+    <h1> Sobre mi</h1>
+    <p><strong>Nombre completo:</strong> Franchelys Batista Meran</p>
+    <img src="img/miFoto.jpeg" alt="Foto 2x2" width="150" height="150"><br>
+    <a href="https://t.me/FrancheBati" target="_blank">Telegram</a><br>
+    <a href="https://wa.me/18094180852" target="_blank">WhatsApp</a><br>
+    
+    <h3> Video que recomiendo</h3>
+    <iframe width="560" height="315" 
+    src="https://www.youtube.com/embed/OF8kTaWWtAI" title="YouTube video" frameborder="0"
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+    allowfullscreen></iframe>
+
+    <div>
+        <a href="index.php" class="volver-btn">Volver al menu</a>
+    </div>
+</body>
+</html>
